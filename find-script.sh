ef223eec667d26c52df383fc4cb0bc41b4c9ebb8
s3242313 <s3242313@student.rmit.edu.au>
2019-10-27 22:01:17 +1100

    Requirement 3 bash script file

--- find-script.sh
+++ find-script.sh
@@ -3,44 +3,99 @@
 # RMIT:		CPT264, Assignment 2, Part B, Requirement 3
 # Author:	Lance Bailey
 # Version:	v1.0
-# Description:	This script runs the below applications based on arguments passed in at runtime:
-#		- df, displays disk space free information in human readable format.
-#		- free, displays memory free information in human readable format.
-#		- ifconfig, displays all network interface information.
-#		- uptime, displays system uptime information.
+# Description:	
+#		- 
 #
 # Change Log:
-#	v1.0	24/10/2019	Lance Bailey
+#	v1.0	27/10/2019	Lance Bailey
 #		Original version.
 #
 
 
 #--- VARIABLES ---
-readonly FILENAME="requirement3"
+
+readonly FILENAME="find-script.sh"
 readonly VERSION="1.0"
 
 readonly FIND="/usr/bin/find"
 
+find_action=""
+find_depth=""
+find_links=""
+find_path=""
+find_type=""
 
 #--- FUNCTONS ---
 
 print_help () {
-	printf "%s\n"		"Usage:"
-	printf "%s\n\n"		" $FILENAME [options]"
-	printf "%s\n"		"Options:"
-	printf "%s\t\t%s\n"	" -h, --help"		"Display this help and exit"
-	printf "%s\t\t%s\n\n"	" -v, --version"	"Display version information"
+	echo "Usage:"
+	echo " $FILENAME [options]"
+	echo ""
+	echo "Options:"
+	echo " -h, --help"	"Display this help and exit"
+	echo " -v, --version"	"Display version information"
+	echo ""
 }
 
 #--- SCRIPT START ---
 
-read -p 'Directory: ' directory
-read -p 'P T G FS: ' pattern
-read -p 'Value :' value
-read -p 'Depth: ' depth
-read -p 'Symb: ' links
-read -p 'Action: ' action
+# Get a valid path, if path is invalid then try again.
+while [ -z $find_path ]
+do
+	read -p 'Enter a path [ENTER]: ' find_path
+	if [ ! -d $find_path ]; then
+		echo 'The path is invalid, please try again.'
+		find_path=''
+	fi
+done
 
+# Get what we are searching for: path, type, group or fstype.
+while [ -z $find_type ]
+do
+	echo 'What would you like to find? Enter the first Letter:'
+	read -p '[F]stype, [G]roup, [P]ath or [T]ype [ENTER]: ' find_type
+	find_type=${find_type,,}
+	if [[ ! $find_type =~ [fgpt] ]]; then
+		echo 'Invalid option, please try again.'
+		find_type=''
+	fi
+done
+
+# Get the value of what is to be searched for.
+while [ -z $find_value ]
+do
+	echo ''
+done
+
+# Get the maximum depth for this search.
+while [ -z $find_depth ]
+do
+	echo''
+done
+
+# Symbolic links
+while [ -z $find_links ]
+do
+	echo 'Do you want to follow symbolic links?'
+	read -p '[Y]es or [N]o [ENTER] ' find_links
+	find_links=${find_links,,}
+	if [[ ! $find_link =~ [ny] ]]
+		echo 'Invalid option, please try again.'
+		find_links=''
+	fi
+done
+
+# Get the action the to be performed on the search.
+while [ -z $find_action ]
+do
+	echo 'What action would you like performed?'
+	read -p '[C]ustom, [D]elete, [P]rint or Print[O] [ENTER]: ' find_action
+	find_action=${find_action,,}
+	if [[ ! $find_action =~ [cdpo] ]]; then
+		echo 'Invalid option, please try again.'
+		find_action=''
+	fi
+done
 
 
 #--- END ---
